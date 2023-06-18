7e14f72c79f643c88acd07a2b18dfa23ae829026
x04000 <78761999+x04000@users.noreply.github.com>
2023-06-18 18:10:11 +0200

    Add files via upload

---
+++ TBOIABTrainer/Form1.Designer.cs
@@ -0,0 +1,223 @@
+﻿namespace TBOIABTrainer
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variable del diseñador necesaria.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpiar los recursos que se estén usando.
+        /// </summary>
+        /// <param name="disposing">true si los recursos administrados se deben desechar; false en caso contrario.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código generado por el Diseñador de Windows Forms
+
+        /// <summary>
+        /// Método necesario para admitir el Diseñador. No se puede modificar
+        /// el contenido de este método con el editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.checkBox1 = new System.Windows.Forms.CheckBox();
+            this.timer1 = new System.Windows.Forms.Timer(this.components);
+            this.checkBox2 = new System.Windows.Forms.CheckBox();
+            this.checkBox3 = new System.Windows.Forms.CheckBox();
+            this.checkBox4 = new System.Windows.Forms.CheckBox();
+            this.comboBox1 = new System.Windows.Forms.ComboBox();
+            this.checkBox5 = new System.Windows.Forms.CheckBox();
+            this.checkBox6 = new System.Windows.Forms.CheckBox();
+            this.checkBox7 = new System.Windows.Forms.CheckBox();
+            this.comboBox2 = new System.Windows.Forms.ComboBox();
+            this.SuspendLayout();
+            // 
+            // checkBox1
+            // 
+            this.checkBox1.AutoSize = true;
+            this.checkBox1.Location = new System.Drawing.Point(31, 28);
+            this.checkBox1.Name = "checkBox1";
+            this.checkBox1.Size = new System.Drawing.Size(73, 17);
+            this.checkBox1.TabIndex = 0;
+            this.checkBox1.Text = "GodMode";
+            this.checkBox1.UseVisualStyleBackColor = true;
+            // 
+            // timer1
+            // 
+            this.timer1.Tick += new System.EventHandler(this.timer1_Tick);
+            // 
+            // checkBox2
+            // 
+            this.checkBox2.AutoSize = true;
+            this.checkBox2.Location = new System.Drawing.Point(31, 61);
+            this.checkBox2.Name = "checkBox2";
+            this.checkBox2.Size = new System.Drawing.Size(92, 17);
+            this.checkBox2.TabIndex = 1;
+            this.checkBox2.Text = "Infinite Bombs";
+            this.checkBox2.UseVisualStyleBackColor = true;
+            // 
+            // checkBox3
+            // 
+            this.checkBox3.AutoSize = true;
+            this.checkBox3.Location = new System.Drawing.Point(31, 94);
+            this.checkBox3.Name = "checkBox3";
+            this.checkBox3.Size = new System.Drawing.Size(83, 17);
+            this.checkBox3.TabIndex = 2;
+            this.checkBox3.Text = "Infinite Keys";
+            this.checkBox3.UseVisualStyleBackColor = true;
+            // 
+            // checkBox4
+            // 
+            this.checkBox4.AutoSize = true;
+            this.checkBox4.Location = new System.Drawing.Point(31, 126);
+            this.checkBox4.Name = "checkBox4";
+            this.checkBox4.Size = new System.Drawing.Size(86, 17);
+            this.checkBox4.TabIndex = 3;
+            this.checkBox4.Text = "Infinite Coins";
+            this.checkBox4.UseVisualStyleBackColor = true;
+            // 
+            // comboBox1
+            // 
+            this.comboBox1.FormattingEnabled = true;
+            this.comboBox1.Items.AddRange(new object[] {
+            "1",
+            "2",
+            "3",
+            "4",
+            "5",
+            "6",
+            "7",
+            "8",
+            "9",
+            "10",
+            "13",
+            "15",
+            "17",
+            "20",
+            "30",
+            "40",
+            "50",
+            "100",
+            "500",
+            "999",
+            "999999"});
+            this.comboBox1.Location = new System.Drawing.Point(31, 208);
+            this.comboBox1.Name = "comboBox1";
+            this.comboBox1.Size = new System.Drawing.Size(104, 21);
+            this.comboBox1.TabIndex = 4;
+            // 
+            // checkBox5
+            // 
+            this.checkBox5.AutoSize = true;
+            this.checkBox5.Location = new System.Drawing.Point(31, 185);
+            this.checkBox5.Name = "checkBox5";
+            this.checkBox5.Size = new System.Drawing.Size(104, 17);
+            this.checkBox5.TabIndex = 5;
+            this.checkBox5.Text = "Change damage";
+            this.checkBox5.UseVisualStyleBackColor = true;
+            // 
+            // checkBox6
+            // 
+            this.checkBox6.AutoSize = true;
+            this.checkBox6.Location = new System.Drawing.Point(170, 28);
+            this.checkBox6.Name = "checkBox6";
+            this.checkBox6.Size = new System.Drawing.Size(115, 17);
+            this.checkBox6.TabIndex = 6;
+            this.checkBox6.Text = "Infinite Blue Hearts";
+            this.checkBox6.UseVisualStyleBackColor = true;
+            // 
+            // checkBox7
+            // 
+            this.checkBox7.AutoSize = true;
+            this.checkBox7.Location = new System.Drawing.Point(170, 185);
+            this.checkBox7.Name = "checkBox7";
+            this.checkBox7.Size = new System.Drawing.Size(93, 17);
+            this.checkBox7.TabIndex = 7;
+            this.checkBox7.Text = "Change Tears";
+            this.checkBox7.UseVisualStyleBackColor = true;
+            // 
+            // comboBox2
+            // 
+            this.comboBox2.FormattingEnabled = true;
+            this.comboBox2.Items.AddRange(new object[] {
+            "0",
+            "1",
+            "2",
+            "3",
+            "4",
+            "5",
+            "6",
+            "7",
+            "8",
+            "9",
+            "10",
+            "11",
+            "12",
+            "13",
+            "14",
+            "15",
+            "16",
+            "17",
+            "18",
+            "19",
+            "20",
+            "21",
+            "22",
+            "23",
+            "24",
+            "25",
+            "50",
+            "100"});
+            this.comboBox2.Location = new System.Drawing.Point(170, 208);
+            this.comboBox2.Name = "comboBox2";
+            this.comboBox2.Size = new System.Drawing.Size(93, 21);
+            this.comboBox2.TabIndex = 8;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.SystemColors.ActiveCaptionText;
+            this.ClientSize = new System.Drawing.Size(400, 490);
+            this.Controls.Add(this.comboBox2);
+            this.Controls.Add(this.checkBox7);
+            this.Controls.Add(this.checkBox6);
+            this.Controls.Add(this.checkBox5);
+            this.Controls.Add(this.comboBox1);
+            this.Controls.Add(this.checkBox4);
+            this.Controls.Add(this.checkBox3);
+            this.Controls.Add(this.checkBox2);
+            this.Controls.Add(this.checkBox1);
+            this.ForeColor = System.Drawing.SystemColors.Control;
+            this.Name = "Form1";
+            this.Text = "The Binding Of Isaac: Antibirth Trainer";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.CheckBox checkBox1;
+        private System.Windows.Forms.Timer timer1;
+        private System.Windows.Forms.CheckBox checkBox2;
+        private System.Windows.Forms.CheckBox checkBox3;
+        private System.Windows.Forms.CheckBox checkBox4;
+        private System.Windows.Forms.ComboBox comboBox1;
+        private System.Windows.Forms.CheckBox checkBox5;
+        private System.Windows.Forms.CheckBox checkBox6;
+        private System.Windows.Forms.CheckBox checkBox7;
+        private System.Windows.Forms.ComboBox comboBox2;
+    }
+}
+
---
+++ TBOIABTrainer/Form1.cs
@@ -0,0 +1,182 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using Memory;
+using static System.Windows.Forms.VisualStyles.VisualStyleElement.Button;
+
+namespace TBOIABTrainer
+{
+    public partial class Form1 : Form
+    {
+        Mem m = new Mem();
+
+        // "zhlRemix2.dll"+0027CF60 + B54
+        string healthAddress = "zhlRemix2.dll+0027CF60,B54";
+        // "zhlRemix2.dll"+0027CF60 + B70
+        string bombsAddress = "zhlRemix2.dll+0027CF60,B70";
+        // "zhlRemix2.dll"+0027CF60 + B68
+        string keysAddress = "zhlRemix2.dll+0027CF60,B68";
+        // "zhlRemix2.dll"+0027CF60 + 5D0 + 38 + 7F8 + B74
+        string coinsAddress = "zhlRemix2.dll+0027CF60,5D0,38,7F8,B74";
+        // "zhlRemix2.dll"+0027CF60 + BF0
+        string damageAddress = "zhlRemix2.dll+0027CF60,BF0";
+        // "zhlRemix2.dll"+0027CF60 + 5D0 + 38 + 7F8 + B5C
+        string blueHeartsAddress = "zhlRemix2.dll+0027CF60,5D0,38,7F8,B5C";
+        // "zhlRemix2.dll"+0027CF60 + BE0
+        string tearsAddress = "zhlRemix2.dll+0027CF60,BE0";
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            int PID = m.GetProcIdFromName("isaac-ng.exe");
+            if (PID > 0)
+            {
+                m.OpenProcess(PID);
+                timer1.Start();
+            }
+            else
+            {
+                MessageBox.Show("TBOI Antibirth is not started!", "Antibirth Trainer",
+                    MessageBoxButtons.OK,
+                    MessageBoxIcon.Error);
+                Application.Exit();
+            }
+        }
+
+        private void timer1_Tick(object sender, EventArgs e)
+        {
+            if (checkBox1.Checked)
+            {
+                m.WriteMemory(healthAddress, "int", "100");
+            }
+            if (checkBox2.Checked)
+            {
+                m.WriteMemory(bombsAddress, "int", "99");
+            }
+            if (checkBox3.Checked)
+            {
+                m.WriteMemory(keysAddress, "int", "99");
+            }
+            if (checkBox4.Checked)
+            {
+                m.WriteMemory(coinsAddress, "int", "99");
+            }
+            if (checkBox5.Checked)
+            {
+                string damageSelected = comboBox1.Text;
+                if (damageSelected == "1")
+                {
+                    m.WriteMemory(damageAddress, "int", "1065353216");
+                }
+                if (damageSelected == "2")
+                {
+                    m.WriteMemory(damageAddress, "int", "1073741824");
+                }
+                if (damageSelected == "3")
+                {
+                    m.WriteMemory(damageAddress, "int", "1077936128");
+                }
+                if (damageSelected == "4")
+                {
+                    m.WriteMemory(damageAddress, "int", "1082130432");
+                }
+                if (damageSelected == "5")
+                {
+                    m.WriteMemory(damageAddress, "int", "1084227584");
+                }
+                if (damageSelected == "6")
+                {
+                    m.WriteMemory(damageAddress, "int", "1086324736");
+                }
+                if (damageSelected == "7")
+                {
+                    m.WriteMemory(damageAddress, "int", "1088421888");
+                }
+                if (damageSelected == "8")
+                {
+                    m.WriteMemory(damageAddress, "int", "1090519040");
+                }
+                if (damageSelected == "9")
+                {
+                    m.WriteMemory(damageAddress, "int", "1091567616");
+                }
+                if (damageSelected == "10")
+                {
+                    m.WriteMemory(damageAddress, "int", "1092616192");
+                }
+                if (damageSelected == "13")
+                {
+                    m.WriteMemory(damageAddress, "int", "1095761920");
+                }
+                if (damageSelected == "15")
+                {
+                    m.WriteMemory(damageAddress, "int", "1097859072");
+                }
+                if (damageSelected == "17")
+                {
+                    m.WriteMemory(damageAddress, "int", "1099431936");
+                }
+                if (damageSelected == "20")
+                {
+                    m.WriteMemory(damageAddress, "int", "1101004800");
+                }
+                if (damageSelected == "30")
+                {
+                    m.WriteMemory(damageAddress, "int", "1106247680");
+                }
+                if (damageSelected == "40")
+                {
+                    m.WriteMemory(damageAddress, "int", "1109393408");
+                }
+                if (damageSelected == "50")
+                {
+                    m.WriteMemory(damageAddress, "int", "1112014848");
+                }
+                if (damageSelected == "100")
+                {
+                    m.WriteMemory(damageAddress, "int", "1120403456");
+                }
+                if (damageSelected == "500")
+                {
+                    m.WriteMemory(damageAddress, "int", "1140457472");
+                }
+                if (damageSelected == "999")
+                {
+                    m.WriteMemory(damageAddress, "int", "1148829696");
+                }
+                if (damageSelected == "999999")
+                {
+                    m.WriteMemory(damageAddress, "int", "1232348144");
+                }
+            }
+            if (checkBox6.Checked)
+            {
+                m.WriteMemory(blueHeartsAddress, "int", "22");
+            }
+            if (checkBox7.Checked)
+            {
+                if (comboBox2.Text != "")
+                {
+                    string tearsSelected = comboBox2.Text;
+                    m.WriteMemory(tearsAddress, "int", tearsSelected);
+                }
+                else
+                {
+                    checkBox7.Checked = false;
+                    MessageBox.Show("Select a value for tears", "Antibirth Trainer",
+                        MessageBoxButtons.OK,
+                        MessageBoxIcon.Warning);
+                }
+            }
+        }
+    }
+}
